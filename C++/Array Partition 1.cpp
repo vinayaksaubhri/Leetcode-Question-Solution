7f1ed18bab383f7efd93f2ac6775339ec729b2e5
Vinayak <vinayaksaubhri@gmail.com>
2020-11-13 15:34:38 +0530

    Adding Array Partiton

--- C++/Array Partition 1.cpp
+++ C++/Array Partition 1.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int arrayPairSum(vector<int>& nums) {
-        std::sort(nums.begin(),nums.end());//sort the array to make pair as the sum have to be maximize the smaller number have to be paired with smaller number and bigger with biggernumber
+        std::sort(nums.begin(),nums.end());//sort the array to make pair as the sum have to be maximize the smaller number have to be paired with smaller number and bigger with bigger number
         int sum=0;
         for(int i=0;i<nums.size();i=i+2){   
             sum=sum+std::min(nums[i],nums[i+1]);
