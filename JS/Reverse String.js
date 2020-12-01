ecfbd5a40569374a9475375aa47feca4424580bc
Vinayak <vinayaksaubhri@gmail.com>
2020-12-01 21:03:33 +0530

    adding Reverse String

---
+++ JS/Reverse String.js
@@ -0,0 +1,15 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var swap = function(a,b){
+    return [b,a]
+}
+var reverseString = function(s) {
+    let start=0,end=s.length-1;
+    while(start<end){
+        [s[start],s[end]]=swap(s[start],s[end]);
+        ++start;
+        --end;
+    }
+};
